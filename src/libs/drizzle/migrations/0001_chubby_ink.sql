bde63d6b9ee59586b2d00292dadeb1e0ff8e0442
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-10-15 01:00:50 +0900

    feature: studentsテーブルを追加

---
+++ src/libs/drizzle/migrations/0001_chubby_ink.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "students" (
+	"id" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"name" varchar NOT NULL,
+	"enrollment_status" integer NOT NULL
+);
--- src/libs/drizzle/schema.ts
+++ src/libs/drizzle/schema.ts
@@ -1,7 +1,14 @@
-import { boolean, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, pgTable, varchar, integer } from "drizzle-orm/pg-core";
 
 export const tasks = pgTable("tasks", {
   id: varchar("id").notNull(),
   title: varchar("title").notNull(),
   done: boolean("done").notNull(),
+});
+
+export const students = pgTable("students", {
+  id: varchar("id").notNull(),
+  email: varchar("email").notNull(),
+  name: varchar("name").notNull(),
+  enrollmentStatus: integer("enrollment_status").notNull(),
 });
